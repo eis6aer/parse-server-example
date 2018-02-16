7e68b947b6fb04c5154590e1e9366cf17365a127
Javier Torres <javier.torres@icomsys.com.mx>
2018-02-16 12:14:15 -0600

    [UPDATED] - Cloud.js

--- cloud/main.js
+++ cloud/main.js
@@ -15,15 +15,26 @@ Parse.Cloud.define('getStates', function(req, res) {
   //query.equalTo("movie", request.params.movie);
   query.find()
     .then((results) => {
-      response.success(sum / results);
+      res.success(sum / results);
     })
     .catch(() =>  {
-      response.error("movie lookup failed");
+      res.error("movie lookup failed");
     });
 });
 
 Parse.Cloud.define('setStates', function (req,res) {
-  var state = Parse.Object.extend('States');
-  state.set("Nombre","Aguascalientes");
-  state.save(null, {useMasterKey = true});
+  var State = Parse.Object.extend('States');
+  var s = new State();
+  s.set("nombre","Aguascalientes");
+  s.save(null, {
+     success: function(state) {
+       // Execute any logic that should take place after the object is saved.
+       alert(‘New object created with objectId: ’ + state.nombre);
+     },
+     error: function(state, error) {
+       // Execute any logic that should take place if the save fails.
+       // error is a Parse.Error with an error code and message.
+       alert(‘Failed to create new object, with error code: ’ + error.message);
+     });
+  res.success("Saved!!");
 });
