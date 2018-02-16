b7b7119d2a8a2d60f9ef7e161497ed321e4bbe9f
Javier Torres <javier.torres@icomsys.com.mx>
2018-02-16 12:08:53 -0600

    [UPDATED] - Query try out

--- cloud/main.js
+++ cloud/main.js
@@ -8,3 +8,22 @@ Parse.Cloud.define('dataFromParse', function(req, res) {
   console.log(req);
   res.success('Consulta realizada! con contenido: ' + req.params.nombre);
 });
+
+
+Parse.Cloud.define('getStates', function(req, res) {
+  const query = new Parse.Query("Review");
+  //query.equalTo("movie", request.params.movie);
+  query.find()
+    .then((results) => {
+      response.success(sum / results);
+    })
+    .catch(() =>  {
+      response.error("movie lookup failed");
+    });
+});
+
+Parse.Cloud.define('setStates', function (req,res) {
+  var state = Parse.Object.extend('States');
+  state.set("Nombre","Aguascalientes");
+  state.save(null, {useMasterKey = true});
+});
